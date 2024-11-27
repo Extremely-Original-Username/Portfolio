0541225735c08882cec7e5c4e7b75c1be5140c11
Extremely_Original_Username <joshinnes05@gmail.com>
2024-11-27 10:45:15 +0000

    fixed casing

--- src/portfolio.client/src/app.tsx
+++ src/portfolio.client/src/app.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './styling/app.css';
-import DynamicBackground from './components/DynamicBackground';
+import DynamicBackground from './components/dynamicBackground';
 
 function App() {
     const [helloWorldText, setHelloWorldText] = useState(''); // State to store the fetched data
